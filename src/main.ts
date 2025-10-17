fbd60d07729502172a5700738220f014fadc2c04
Alfredo Barragan <64043214+PastaChicken@users.noreply.github.com>
2025-10-17 02:03:28 +0000

    Co-authored-by: adegan1 <adegan1@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,7 @@ import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
 document.body.innerHTML = `
+ <h1> Insert Title Here </h1>
   <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
+  <canvas class="canvaspad" width="256" height="256"></canvas>
 `;
